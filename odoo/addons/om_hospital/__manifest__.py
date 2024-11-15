0a4bcd09f7bd0df74ad6fc2df2903a084af651ec
Dominik Banaszak <d.banaszak@laboprint.eu>
2024-11-15 12:49:15 +0100

    adding chatter

--- odoo/addons/om_hospital/__manifest__.py
+++ odoo/addons/om_hospital/__manifest__.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Hospital',
     'author': 'dbanaszak',
@@ -6,6 +9,7 @@
     'installable': True,
     'application': True,
     'auto_install': False,
+    'depends': ['mail'],
     'data': [
         'security/ir.model.access.csv',
         'views/menu.xml',
--- odoo/addons/om_hospital/models/patient.py
+++ odoo/addons/om_hospital/models/patient.py
@@ -4,11 +4,18 @@ from pkg_resources import require
 
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
+    _inherit = 'mail.thread'
     _description = "Odoo practice model"
 
-    name = fields.Char(string='Name', required = True)
-    surname = fields.Char(string ='Surname', required = True)
-    age = fields.Integer(string = "Age")
+    name = fields.Char(string='Name', required = True, tracking = True)
+    surname = fields.Char(string ='Surname', required = True, tracking = True)
+    age = fields.Integer(string = "Age", tracking = True)
     is_child = fields.Boolean(string = "Patient is a child")
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string = "Gender")
-    
+
+    @api.onchange('age')
+    def  onchange_age(self):
+        if self.age < 18:
+            self.is_child = True
+        else:
+            self.is_child = False
